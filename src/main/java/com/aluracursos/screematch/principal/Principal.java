7bda2895637851f9a8d6676e29bebd82f7a3f638
xxharry19xx <hssilvavasquez@gmail.com>
2025-01-28 20:48:42 -0500

    subiendo avances

--- src/main/java/com/aluracursos/screematch/principal/Principal.java
+++ src/main/java/com/aluracursos/screematch/principal/Principal.java
@@ -7,10 +7,9 @@ import com.aluracursos.screematch.model.Episodio;
 import com.aluracursos.screematch.service.ConsumoApi;
 import com.aluracursos.screematch.service.ConvierteDatos;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Principal {
@@ -47,11 +46,16 @@ public class Principal {
                 .collect(Collectors.toList());
 
         //TOP 5 EPISODIOS
-        datosEpisodios.stream()
-                .filter(e -> !e.evaluacion().equalsIgnoreCase("N/A"))
-                .sorted(Comparator.comparing(DatosEpisodio::evaluacion).reversed())
-                .limit(5)
-                .forEach(System.out::println);
+//        datosEpisodios.stream()
+//                .filter(e -> !e.evaluacion().equalsIgnoreCase("N/A"))
+//                .peek(e -> System.out.println("Primer Filtro de  (N/A)" + e))
+//                .sorted(Comparator.comparing(DatosEpisodio::evaluacion).reversed())
+//                .peek(e -> System.out.println("Segundo filtro ordenar de mayoor a menor"))
+//                .limit(5)
+//                .peek(e -> System.out.println())
+//                .forEach(System.out::println);
+
+
 
         // conviertiendo los datos en tipo episodio
         List<Episodio> episodios = temporadas.stream()
@@ -60,6 +64,62 @@ public class Principal {
                 .collect(Collectors.toList());
 
         episodios.forEach(System.out::println);
+
+        //busqueda de episodio apartir de la fecha
+        System.out.println("Ingresar una fecha");
+        var fecha = teclado.nextInt();
+
+        //LocalDate.of(fecha, 1, 1): Crea una fecha específica utilizando el año proporcionado por el usuario,
+        // con el mes y día fijados en enero 1.
+        LocalDate fechaBusqueda = LocalDate.of(fecha, 1, 1);
+
+        //DateTimeFormatter.ofPattern("dd/MM/yyyy"): Define un formato para mostrar fechas como día/mes/año.
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+//        episodios.stream()
+//                //e.getFechaDeLanzamiento() != null: Verifica que el episodio tenga una fecha de lanzamiento válida (no sea null).
+//                //e.getFechaDeLanzamiento().isAfter(fechaBusqueda): Verifica que la fecha de lanzamiento del episodio sea posterior a la fecha ingresada por el usuario (fechaBusqueda).
+//                .filter(e -> e.getFechaDeLanzamiento() != null && e.getFechaDeLanzamiento().isAfter(fechaBusqueda))
+//                .forEach(e -> System.out.println(
+//                        "Temporada " + e.getTemporada() +
+//                                "Episodio" + e.getTitulo() +
+//                                "Fecha de Lamzamientio" + e.getFechaDeLanzamiento().format(dtf)
+//                        ));
+
+        //Buscar Los Episodios segun su nombre
+//        System.out.println("Ingresa el nombre de la pelicula");
+//        teclado.nextLine();  // Esto asegura que nextLine() no lea un salto de línea vacío
+//        String titulutop = teclado.nextLine();
+//
+//        episodios.stream()
+//                .filter(e -> e.getTitulo().equalsIgnoreCase(titulutop))
+//                .forEach(e -> System.out.println(
+//                        "Episodio" + e.getTitulo() +
+//                                "Fecha de Lamzamientio" + e.getFechaDeLanzamiento().format(dtf)
+//                ));
+
+
+        // busca episodios por pedasos de titulo
+        System.out.println("Ingresa el nombre de la pelicula");
+        teclado.nextLine();  // Esto asegura que nextLine() no lea un salto de línea vacío
+        String pedasoTitulo = teclado.nextLine();
+
+        //Declara una variable de tipo Optional<Episodio> que almacenará el resultado de la búsqueda.
+        Optional<Episodio> tituloBuscado = episodios.stream()
+
+//              .contains(...): Verifica si el título del episodio contiene la subcadena buscada.
+                //toUpperCase() convierte la cadena en mayusculas
+                .filter(e -> e.getTitulo().toUpperCase().contains(pedasoTitulo.toUpperCase()))
+
+                //Devuelve el primer episodio que cumpla con la condición del filtro.
+                .findFirst();
+
+        if (tituloBuscado.isPresent()){
+            System.out.println("Titulo encontrado : ");
+            System.out.println("los datos son : " + tituloBuscado.get());
+        }else {
+            System.out.println("el episodio no se encuentra presente");
+        }
 
     }
 }
